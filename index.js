95a42d9f74217528d9f529bfb2d37b3834932b83
Icaro Abreu Johann <icaroabreujohann07@gmail.com>
2024-04-30 15:26:36 -0300

    Finalizada

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 let taskInput = document.getElementById('taskInput')
 let taskAdd = document.getElementById('taskAdd')
 let listUL = document.getElementById('listUL')
-let indexTaskObj = 0
+let taskNotesContainer = document.getElementById('taskNotesContainer')
 
+let indexTaskObj = 0
 let taskList = []
 
 taskAdd.addEventListener('click', function () {
@@ -16,7 +17,7 @@ function reloadTaskList() {
 
             <p class="taskItem">${task.title}</p>
         
-            <button class="taskNoteBtn" value="${taskList.indexOf(task)}" onclick="openNotesTask(this.value)">
+            <button class="taskNoteBtn" value="${taskList.indexOf(task)}" onclick="openNote(this.value)">
                 <img class="iconNotes" src="/Icons/iconNotes.svg">
             </button>
 
@@ -24,14 +25,17 @@ function reloadTaskList() {
                 <img class="iconDelete" src="/Icons/deleteIcon.svg">
             </button>
             <!-- Notes -->
-            <div class="mainTaskContainer">
+            <div class="mainTaskContainer" id="taskId${taskList.indexOf(task)}" >
  
             <div class="titleTaskSect">
-                <p class="noteTitleTask">${task.title}</p>
+                <input class="noteTitleTask" value="${task.title}" spellcheck="false" id="taskTitle${taskList.indexOf(task)}">
+                <!--<p class="noteTitleTask">${task.title}</p>-->
                 <button class="noteCloseBtn" value="${taskList.indexOf(task)}" onclick="closeNotesTask(this.value)">
                     <img class="iconCloseTask" src="/Icons/iconClose.svg" alt="CloseNote">
                 </button>
             </div>
+            <textarea name="noteTextTask" id="task${taskList.indexOf(task)}" class="noteTextTask" rows="10" spellcheck="false" placeholder="Add Your Note">${task.text}</textarea>
+
             </div>
             
         </div>
@@ -39,26 +43,8 @@ function reloadTaskList() {
     })
     taskListMap = taskListMap.join("")
     listUL.innerHTML = taskListMap
-
-    console.log(taskList)
-
+    console.table(taskList)
 }
-
-/*function addTaskOnList() {
-    if (taskInput.value === "") {
-        window.alert('Insira')
-    } else {
-        taskList.push(taskInput.value)
-
-        taskInput.value = ""
-        taskInput.focus()
-
-
-        let taskIndex = 0
-        taskIndex++
-        reloadTaskList()
-    }
-}*/
 
 function addTaskOnList() {
     if (taskInput.value === "") {
@@ -67,6 +53,7 @@ function addTaskOnList() {
 
         taskList.push({ id: indexTaskObj, })
         taskList[indexTaskObj].title = taskInput.value
+        taskList[indexTaskObj].text = ""
 
         taskInput.value = ""
         taskInput.focus()
@@ -76,7 +63,7 @@ function addTaskOnList() {
         taskIndex++
         indexTaskObj++
         reloadTaskList()
-        console.table(taskList)
+
     }
 }
 
@@ -86,5 +73,28 @@ function deleteTask(index) {
 
 }
 
+
+
 // NOTES SECTION
 
+function openNote(index) {
+    let id = `taskId${index}`
+    let currentNoteBlock = document.querySelector(`div#${id}`)
+    currentNoteBlock.classList.add("active")
+}
+
+function closeNotesTask(index) {
+    let currentNote = document.getElementById(`task${index}`).value;
+    taskList[index].text = currentNote
+
+    let id = `taskId${index}`
+    let currentNoteBlock = document.querySelector(`div#${id}`)
+    currentNoteBlock.classList.remove("active")
+
+    let currentNoteTitle = document.getElementById(`taskTitle${index}`).value
+    taskList[index].title = currentNoteTitle
+
+    reloadTaskList()
+    console.table(taskList)
+
+}
